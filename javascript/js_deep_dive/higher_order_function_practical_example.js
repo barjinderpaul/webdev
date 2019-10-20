ab87373ea4f5123d4f8a91df943a91aba1f0395b
barjinderpaul <barjinderpaul.singh@gmail.com>
2019-10-21 01:53:41 +0530

    Added explanation for higher_order_function_practical_example.js

--- javascript/js_deep_dive/higher_order_function_practical_example.js
+++ javascript/js_deep_dive/higher_order_function_practical_example.js
@@ -3,13 +3,14 @@
 function createCounter (intValue, delta) {
     let val = intValue
     function counter() {
+        // console.log(`Value of val = ${val}`)
         val = val + delta;
         return val;
     }
     return counter;
 }
 
-// Value of val does not die because of closures;
+// Value of val does not die because of closure scope;
 // Till the fiveCount is in memory, it will retain the value of val;
 // When we run fiveCount, only the inner function counter runs and not the ourer scope;
 // So, when we call fiveCount() again and again, only the counter works, which increases the value by delta;
@@ -19,3 +20,23 @@ console.log(fiveCount());
 console.log(fiveCount());
 console.log(fiveCount());
 
+
+
+function createTimeStamps(timeStampsArr, currentTimeStamp){
+    let currTime = currentTimeStamp;
+    function pushTimeStamp() {
+        timeStampsArr.push(timeStampsArr[timeStampsArr.length-1]+currTime);
+        return timeStampsArr;
+    }
+    return pushTimeStamp;
+}
+
+let timeStamps = []
+timeStamps.push(Date.now())
+
+
+let seeTimeStamps = createTimeStamps(timeStamps,Date.now());
+
+console.log(seeTimeStamps())
+console.log(seeTimeStamps())
+console.log(seeTimeStamps())
