d4326e6989e4d5f44f7db6edd1162a193ffb1775
barjinderpaul <barjinderpaul.singh@gmail.com>
2019-10-23 18:10:53 +0530

    Added more about iife(Immediately Invoked function expression)

--- javascript/js_deep_dive/iife.js
+++ javascript/js_deep_dive/iife.js
@@ -18,4 +18,41 @@
 
 (function(l){
     l("hello world")
+})(console.log);
+
+
+(function(print){
+    print("Hello world print")
 })(console.log)
+
+
+// For loop set timeout with iife;
+// Will print 10, 10 times;
+// can be solved by iife as well as using let
+for (var i=0;i<10;++i){
+    setTimeout(function(){
+        console.log(i)
+    },100)
+}
+
+// Solving with iife;
+
+for(var i=0;i<10;i++){
+    (function(j){
+        setTimeout(function(){
+            console.log(j)
+        },100)
+    })(i)
+}
+
+// Solving with let;
+for(let i=0;i<10;i++){
+    setTimeout(() => {
+        console.log(i)
+    }, 100);
+}
+
+// solving for es5 users;
+for(var i=0;i<10;i++){
+    setTimeout(console.log,100,i)
+}
