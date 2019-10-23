e6617b515b2f37570855dc2a924e3fe2edabe85a
barjinderpaul <barjinderpaul.singh@gmail.com>
2019-10-23 18:31:43 +0530

    Understanding Javascript's Call Stack

---
+++ javascript/js_deep_dive/javascript_callstack.js
@@ -0,0 +1,25 @@
+function ceo() {
+    console.log(`Lets create something`)
+    cto()
+}
+
+function cto(){
+    console.log(`Lets make a product`)
+    vpEngg()
+}
+
+function vpEngg(){
+    console.log(`Lets make a new JS framework`)
+    techLead()
+}
+
+function techLead(){
+    console.log(`Lets port React to new_react`)
+    developer()
+}
+
+function developer() {
+    console.log(`I am building it`)
+}
+
+ceo();
