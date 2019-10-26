c2d56faa280f942d5595678266d010023d0ffe83
barjinderpaul <barjinderpaul.singh@gmail.com>
2019-10-26 13:56:57 +0530

    Added custom functions for map and filter

---
+++ javascript/custom_functional_programming/custom_map_filter.js
@@ -0,0 +1,38 @@
+
+let nums = [1,2,3,4,5]
+
+// Custom map function
+Array.prototype.myMap = function(callback) {
+    let newNums = []
+    for(let i=0;i<this.length;++i ){
+        newNums.push(callback(this[i]))
+    }
+    return newNums
+}
+
+let newNums = nums.myMap((num)=>{
+    return num*2
+})
+
+console.log(nums)
+console.log(newNums)
+
+
+
+// custom filter function
+Array.prototype.myFilter = function(callback) {
+    let filteredArray = []
+
+    for(let i=0;i<this.length;++i) {
+        if(callback(this[i])) {
+            filteredArray.push(this[i])
+        }
+    }
+
+    return filteredArray
+}
+
+let filteredNums = nums.myFilter( num => !(num&1) )
+console.log(nums)
+
+console.log(filteredNums)
